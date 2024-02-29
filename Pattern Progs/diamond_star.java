2ed5e026c7cf50fea51570234de7e95fb0ddddd4
Akshat Srivastava <107216804+Akshat-Sri7@users.noreply.github.com>
2024-02-29 20:30:54 +0530

    Create diamond_star.java

---
+++ Pattern Progs/diamond_star.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class diamond_star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N:");
+        int N = sc.nextInt();
+        for(int i = 0 ; i < N ;i++){
+            for (int j = 0; j <= N-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2*i+1; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j <= N-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i = N-1; i >= 0; i--){
+            for(int j = 0; j<= N-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = 2*i+1; j > 0; j--){
+                System.out.print("*");
+            }
+            for(int j = 0; j<= N-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
